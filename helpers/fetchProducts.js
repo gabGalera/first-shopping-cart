c81e4b3c9762601b858802cd3bc36dd5a6bdf439
Gabriel Galera <gabgalera@hotmail.com>
2022-09-12 17:43:04 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,20 @@
-const fetchProducts = () => {
+const fetchAPI = (QUERY) => fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
+
+const fetchProducts = async (QUERY) => { 
   // seu código aqui
+  try {
+    if (typeof QUERY === 'undefined') {
+      throw new Error('Raiva');
+    }
+    const getAPI = await fetchAPI(QUERY);
+    const response = await getAPI.json();
+    return response;
+  } catch (error) {
+    throw await error.message;
+  }
 };
+
+// console.log(fetchProducts()); 
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -1,8 +1,33 @@
 require('../mocks/fetchSimulator');
+
+jest.setTimeout(10000);
+
+const { expect } = require('@jest/globals');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('is a function', () => {
+    expect(typeof fetchProducts).toBe('function')
+  })
+
+  test('if the fetch function was called', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
+
+  it('has the right endpoint', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  test('if fetchProducts structure matches computadorSearch', async () => {
+    const report = await fetchProducts('computador');
+    expect(report).toEqual(computadorSearch);
+  })
+
+  test('if an empty url throws an error', async () => {
+    await expect(fetchProducts()).rejects.toBe('Raiva');
+  })
 });
